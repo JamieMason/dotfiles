675c5e03d20be1e71f03afa326724464aa930b05
Jamie Mason <jamie@foldleft.io>
2019-06-02 19:20:24 +0100

    Remove homebrew/homebrew-dupes

--- brew.sh
+++ brew.sh
@@ -38,9 +38,6 @@ brew install wget --with-iri
 
 # Install more recent versions of some macOS tools.
 brew install vim --override-system-vi
-brew install homebrew/dupes/grep
-brew install homebrew/dupes/openssh
-brew install homebrew/dupes/screen
 
 # Install other useful binaries.
 brew install duti
