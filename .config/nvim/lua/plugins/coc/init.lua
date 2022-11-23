7685cf439917e8f3d2868c26284b26c1b0ed4681
Jamie Mason <jamie@foldleft.io>
2022-11-23 15:04:42 +0000

    Remove unused code

--- .config/nvim/lua/plugins/coc/init.lua
+++ .config/nvim/lua/plugins/coc/init.lua
@@ -20,14 +20,4 @@ vim.cmd([[
 ]])
 
 -- Use K to show documentation in preview window.
--- vim.cmd([[
---   nnoremap <silent> K :call <SID>show_documentation()<CR>
---
---   function! s:show_documentation()
---     if (index(['vim','help'], &filetype) >= 0)
---       execute 'h '.expand('<cword>')
---     else
---       call CocAction('doHover')
---     endif
---   endfunction
--- ]])
+
