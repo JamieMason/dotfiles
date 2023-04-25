a3997aa4cc13ace290048d73bfcbc3eab2ff7ce4
Jamie Mason <jamie@foldleft.io>
2023-04-25 08:40:50 +0100

    Use @antfu/ni

--- 06-install-npm-binaries.sh
+++ 06-install-npm-binaries.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-npm install -g commit-release
-npm install -g http-server
-npm install -g npm-check-updates
-npm install -g organize-imports-cli
-npm install -g prettier
-npm install -g svgo
-npm install -g syncpack
-npm install -g yarn
-
+npm install -g @antfu/ni \
+  commit-release \
+  http-server \
+  npm-check-updates \
+  organize-imports-cli \
+  prettier \
+  svgo \
+  syncpack \
+  yarn
