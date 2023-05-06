980336ccd8d0ca920f93e9792fc6ef2f9952a5ea
Jamie Mason <jamie@foldleft.io>
2023-05-06 12:46:29 +0100

    Fix skhdrc setup

--- 09-install-skhdrc.sh
+++ 09-install-skhdrc.sh
@@ -3,5 +3,6 @@
 # install https://github.com/koekeishiya/skhd
 brew install koekeishiya/formulae/skhd
 cp ./home/skhdrc ~/.skhdrc
+mkdir -p ~/Library/LaunchAgents
 skhd --install-service
 skhd --start-service
