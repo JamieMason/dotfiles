aaaef4c944b7a8bd88c8e1d326c3fbd3feb3860a
Jamie Mason <jamie@foldleft.io>
2023-05-22 07:58:46 +0100

    Update null_ls auto install

--- nvim/lua/custom/configs/null-ls.lua
+++ nvim/lua/custom/configs/null-ls.lua
@@ -1,4 +1,4 @@
-local present, null_ls = pcall(require, "null-ls")
+local present, null_ls = pcall(require, 'null-ls')
 
 if not present then
   return
@@ -7,19 +7,26 @@ end
 local b = null_ls.builtins
 
 local sources = {
-
-  -- webdev stuff
-  b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
-
+  b.formatting.prettier.with({
+    filetypes = {
+      'astro',
+      'css',
+      'graphql',
+      'html',
+      'javascript',
+      'javascriptreact',
+      'json',
+      'markdown',
+      'typescript',
+      'typescriptreact',
+      'yaml',
+    },
+  }), -- so prettier works only on these filetypes
   -- Lua
   b.formatting.stylua,
-
-  -- cpp
-  b.formatting.clang_format,
 }
 
-null_ls.setup {
+null_ls.setup({
   debug = true,
   sources = sources,
-}
+})
