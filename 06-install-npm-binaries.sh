567ff9918e4fd4cbef0d95af3182fdc5cef78eaa
Jamie Mason <jamie@foldleft.io>
2023-12-22 08:18:31 +0000

    Update global npm deps

--- 06-install-npm-binaries.sh
+++ 06-install-npm-binaries.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-npm install -g @antfu/ni \
+npm install -g \
+  @antfu/ni \
   commit-release \
   http-server \
+  knip \
+  npm \
   npm-check-updates \
   organize-imports-cli \
   prettier \
   svgo \
   syncpack \
+  tsx \
   yarn
