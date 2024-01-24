4e8bac833e12ea1c15318843b99a284a86c63f2e
Jamie Mason <jamie@foldleft.io>
2024-01-24 10:35:35 +0000

    Reset nvim fully

--- 08-copy-dotfiles.sh
+++ 08-copy-dotfiles.sh
@@ -5,6 +5,8 @@ mkdir -p ~/.config
 
 # Replace Neovim setup
 rm -rf ~/.config/nvim
+rm -rf ~/.local/share/nvim
+rm -rf ~/.cache/nvim
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 cp -r ./nvim/lua/custom ~/.config/nvim/lua
 
