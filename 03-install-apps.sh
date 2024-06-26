7939f58e7772ea05b4ee54fc974bc0522eb710d4
Jamie Mason <jamie@foldleft.io>
2024-06-26 16:38:42 +0100

    Switch to Zed

--- 03-install-apps.sh
+++ 03-install-apps.sh
@@ -15,10 +15,10 @@ brew install iterm2
 brew install keycastr
 brew install spectacle
 brew install spotify
-brew install sublime-text-dev
 brew install tower
 brew install visual-studio-code
 brew install xbar
+brew install zed
 
 # Remove outdated versions from the cask cellar.
 brew cleanup
