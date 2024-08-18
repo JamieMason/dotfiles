bd6391ede7597d7ce97bbf9ad4b2e783524d08fd
Jamie Mason <jamie@foldleft.io>
2024-08-18 08:21:09 +0100

    Remove koekeishiya/formulae/skhd

--- 09-install-skhdrc.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# install https://github.com/koekeishiya/skhd
-brew install koekeishiya/formulae/skhd
-cp ./home/skhdrc ~/.skhdrc
-mkdir -p ~/Library/LaunchAgents
-skhd --install-service
-skhd --start-service
