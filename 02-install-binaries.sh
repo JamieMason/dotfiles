9d81fa7919d0b8cee60dcc66807f2bcac4e31ac2
Jamie Mason <jamie@foldleft.io>
2025-02-04 09:50:54 +0000

    Update brew formulae

--- 02-install-binaries.sh
+++ 02-install-binaries.sh
@@ -13,10 +13,20 @@ brew update
 brew upgrade
 
 # Install binaries
+brew install act
+brew install ast-grep
+brew install cargo-dist
+brew install docker
 brew install duti
 brew install fd
+brew install fnm
+brew install fonttools
+brew install gh
 brew install git
+brew install graphviz
 brew install hub
+brew install imagemagick
+brew install jq
 brew install neovim
 brew install ripgrep
 brew install tree
@@ -26,3 +36,5 @@ brew install wget
 # Remove outdated versions from the cellar.
 brew cleanup
 
+# https://stackoverflow.com/q/71591971/745158#comment127364294_71621142
+ln -s /usr/local/bin/python3 /usr/local/bin/python
